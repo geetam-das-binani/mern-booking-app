feff9d83e40a064fb128f551e285ce2bb3cdfbe2
Geetam <geetambinani6@gmail.com>
2024-03-21 12:41:59 +0530

    add hotel add feature

--- backend/src/controllers/hotels.controllers.ts
+++ backend/src/controllers/hotels.controllers.ts
@@ -3,7 +3,7 @@ import { catchAsyncErrors } from "../utils/catchAsyncErrors";
 import { handleImagesUploads } from "../cloudinary/cloudinary";
 import { HotelType } from "../models/hotel";
 import Hotel from "../models/hotel";
-
+import { v2 as cloudinary } from "cloudinary";
 import { ErrorHandler } from "../utils/error";
 
 const createHotel = catchAsyncErrors(
@@ -22,10 +22,19 @@ const createHotel = catchAsyncErrors(
 
     //! upload the images to cloudinary
 
-    const uploadedFiles = await handleImagesUploads(imageFiles)(req, res, next);
+    const uploadPromises = await Promise.all(
+      imageFiles.map(async (imageFile) => {
+        const b64 = Buffer.from(imageFile.buffer).toString("base64");
+        let dataURI = `data:${imageFile.mimetype};base64,${b64}`;
+        const res = await cloudinary.uploader.upload(dataURI, {
+          folder: "hotelapp",
+        });
+        return res.secure_url;
+      })
+    );
 
     hotel.userId = req.user;
-    hotel.imageUrls = uploadedFiles as string[];
+    hotel.imageUrls = uploadPromises as string[];
     hotel.lastUpdated = new Date();
 
     const newHotel = await Hotel.create(hotel);
