e2d64d8b69280bb38b5f1584ede2f37ef888e454
Geetam <geetambinani6@gmail.com>
2024-03-22 10:54:12 +0530

    add hotels view page

--- backend/src/shared/types.ts
+++ backend/src/shared/types.ts
@@ -1,18 +1,18 @@
-import { Schema } from "mongoose";
+import mongoose from "mongoose";
 
 export type HotelType = {
-    _id: string;
-    userId:typeof Schema.Types.ObjectId;
-    name: string;
-    country: string;
-    desc: string;
-    city: string;
-    type: string;
-    adultCount: number;
-    childCount: number;
-    imageUrls: string[];
-    facilities: string[];
-    pricePerNight: number;
-    starRating: number;
-    lastUpdated: Date;
-  };
+  _id: string;
+  userId: typeof mongoose.Schema.Types.ObjectId;
+  name: string;
+  country: string;
+  desc: string;
+  city: string;
+  type: string;
+  adultCount: number;
+  childCount: number;
+  imageUrls: string[];
+  facilities: string[];
+  pricePerNight: number;
+  starRating: number;
+  lastUpdated: Date;
+};
